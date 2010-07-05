268d29e7de5a43e2344a7ee2b72b39f1e3b4e6ef
Mike Boers <github@mikeboers.com>
2010-07-05 10:24:08 -0400

    Created setup file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name='PyHAML',
+    version='0.1a',
+    description='Pythonic implementation of HAML, cross compiling to Mako template syntax.',
+    url='http://github.com/mikeboers/PyHAML',
+    author='Mike Boers',
+    author_email='PyHAML@mikeboers.com',
+    license='New BSD License',
+    packages=['haml'],
+)
