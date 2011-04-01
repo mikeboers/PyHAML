07d4c52fb7a75615db97914659b99b4cba9c78e9
Mike Boers <github@mikeboers.com>
2011-04-01 11:10:51 -0400

    0.1.3 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='PyHAML',
-    version='0.1.2',
+    version='0.1.3',
     description='Pythonic implementation of HAML, cross compiling to Mako template syntax.',
     url='http://github.com/mikeboers/PyHAML',
     author='Mike Boers',
