8d978251869858bccfa1ae1a78a1e43dfa355ea7
Mike Boers <github@mikeboers.com>
2011-11-27 08:18:48 -0800

    0.1.5 release.
    Includes:
    - if/elif/else.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='PyHAML',
-    version='0.1.4',
+    version='0.1.5',
     description='Pythonic implementation of HAML, cross compiling to Mako template syntax.',
     url='http://github.com/mikeboers/PyHAML',
     
